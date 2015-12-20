13e60bc651cae1262b2c514374abcbee56632d1b
Bryan Tong <fangedgamer@gmail.com>
2015-12-19 22:00:41 -0800

    Fixed buttons

---
+++ Assets/Scripts/SceneChecking.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChecking : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeSceneTo( int sceneNum){
+		Application.LoadLevel (sceneNum);
+	}
+
+	public void ChangeSceneTo( string sceneName){
+		Application.LoadLevel (sceneName);
+	}
+}
