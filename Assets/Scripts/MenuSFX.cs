1ed17e42c0645edb9660ebb132ea67713f68a5d9
ecsit <ecsit39@gmail.com>
2016-01-04 17:48:16 -0500

    New Title Screen Nav and Volume! (unfinished)

--- Assets/Scripts/MenuSFX.cs
+++ Assets/Scripts/MenuSFX.cs
@@ -1,19 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MenuSFX : MonoBehaviour {
 
     private Master m;
 
+
 	// Use this for initialization
 	void Start () {
         m = GameObject.FindGameObjectWithTag("Master").GetComponent<Master>();
-        
+        //GetComponent<Slider>().onValueChanged.
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Up") || Input.GetButtonDown("Right"))
+        if (Input.GetButtonDown("Up") || Input.GetButtonDown("Right")
+            || Input.GetButtonDown("Left") || Input.GetButtonDown("Down"))
         {
             m.PlaySFX(1);
         }
