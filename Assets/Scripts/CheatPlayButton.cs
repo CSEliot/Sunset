5bc134023d754a3989a04df0a94dc740247eab1f
ecsit <ecsit39@gmail.com>
2016-05-04 19:05:17 -0400

    HEYO Forgot to add cheat buttons

---
+++ Assets/Scripts/CheatPlayButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheatPlayButton : MonoBehaviour {
+
+    private int totalPresses;
+    public int CheatPressCount;
+    private ReadyUp rdyScript;
+
+	// Use this for initialization
+	void Start () {
+        rdyScript = GameObject.FindGameObjectWithTag("ReadyOBJ").GetComponent<ReadyUp>();
+        totalPresses = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void CHEAT()
+    {
+        totalPresses++;
+        if(totalPresses > CheatPressCount)
+        {
+            totalPresses = 0;
+            rdyScript.CheatGame();
+        }
+    }
+}
