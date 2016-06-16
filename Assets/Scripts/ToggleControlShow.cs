75b8631c3a44cfa70f534c3f46cdbfb3e4392886
ecsit <ecsit@localhost>
2016-06-15 22:47:09 -0400

    NON-MAJOR

---
+++ Assets/Scripts/ToggleControlShow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleControlShow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Show() {
+        GameObject.FindGameObjectWithTag("Master").GetComponent<Master>().IsControlsShown = !GameObject.FindGameObjectWithTag("Master").GetComponent<Master>().IsControlsShown;
+    }
+}
