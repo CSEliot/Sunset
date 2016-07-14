19884a59920b9c7ce2a08a385f37d23551d887ef
Eliot <ecsit39@gmail.com>
2016-07-14 15:34:52 -0700

    NON-MAJOR

---
+++ Assets/Scripts/ACTIVATEGAME.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ACTIVATEGAME : MonoBehaviour {
+
+    public GameObject BG;
+    public GameObject MB;
+    public AudioSource A;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GO ()
+    {
+        BG.SetActive(true);
+        MB.SetActive(true);
+        A.time = 8;
+        gameObject.SetActive(false);
+    }
+}
