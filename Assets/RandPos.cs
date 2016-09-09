5727dcc1c576deddfc2d546969c693d90f381af4
Eliot Carney-Seim <ecsit39@gmail.com>
2016-09-09 18:58:45 -0400

    NON-Major Arena stage animations.

---
+++ Assets/RandPos.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandPos : MonoBehaviour {
+
+    public Vector3 MinValues;
+
+    public Vector3 MaxValues;
+
+    private Vector3 newPos = new Vector3();
+
+    // Use this for initialization
+    void Start () {
+
+        newPos = new Vector3();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Randomize()
+    {
+        if(Master.DEBUG_ON)
+            Debug.Log("Randomizing! Skelly Position");
+        newPos.Set(Random.Range(MinValues.x, MaxValues.x), Random.Range(MinValues.y, MaxValues.y), Random.Range(MinValues.z, MaxValues.z));
+        transform.localPosition = newPos;
+    }
+}
