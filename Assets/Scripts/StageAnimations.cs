3ae41f07ede8e290b19f3a87d1a1f1508684b0b5
Eliot Carney-Seim <ecsit39@gmail.com>
2016-10-05 17:35:32 -0400

    NON-MAJOR backing up animation syncing

---
+++ Assets/Scripts/StageAnimations.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class StageAnimations : MonoBehaviour {
+
+    private Animator localAnim;
+
+	// Use this for initialization
+	void Start () {
+        localAnim = GetComponent<Animator>();
+        localAnim.enabled = false;
+        gameObject.tag = "StageAnim";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static void Activate()
+    {
+        GameObject[] localAnims = GameObject.FindGameObjectsWithTag("StageAnim");
+        if (localAnims == null || localAnims.Length == 0)
+            return;
+
+        for(int x = 0; x < localAnims.Length; x++) {
+            localAnims[x].GetComponent<StageAnimations>().ActivateLocal();
+        }
+    }
+
+    public void ActivateLocal()
+    {
+        localAnim.enabled = true;
+    }
+
+
+}
