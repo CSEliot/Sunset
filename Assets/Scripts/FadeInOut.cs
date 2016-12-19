b2d9690e8d93e5ed6b338ed79f321fd9f9d186a1
Eliot Carney-Seim <eliot@kitelion.me>
2016-12-18 22:34:08 -0500

    Pushing latest changes, practice mode updated, but not working still.

--- Assets/Scripts/FadeInOut.cs
+++ Assets/Scripts/FadeInOut.cs
@@ -4,26 +4,36 @@ using System.Collections;
 
 
 /// <summary>
-/// Specifically for fading TEXT in and out. Sorry not sorry.
+/// Specifically for fading TEXT in and out. 
 /// </summary>
+[RequireComponent( typeof(Text))]
 public class FadeInOut : MonoBehaviour {
 
+    [Range(0.0f, 1.0f)]
     public float FadeSpeed = 0.5f;
 
+    private Text targetText;
+    private float alpha;
     
 
 	// Use this for initialization
 	void Start () {
-	
+
+        targetText = GetComponent<Text>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        alpha = Mathf.Sin(Time.time * FadeSpeed);
+
+        targetText.color = new Color(
+            targetText.color.r,
+            targetText.color.g,
+            targetText.color.b,
+            alpha
+            );
+
 	}
-
-    public void Activate()
-    {
-
-    }
 }
